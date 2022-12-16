2ac382876f25ce5846ac42b27ea3bdac26b9fe14
Tomasz Bawor <bawortomasz@gmail.com>
2022-12-16 18:39:27 +0100

    Add navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <i-layout>
-    <i-layout-header>
-      Header
-    </i-layout-header>
-
-    <i-layout-content>
-      Content
-    </i-layout-content>
-  </i-layout>
+  <NavBar />
 </template>
+<script setup lang="ts">
+import NavBar from "../components/layout/NavBar";
+</script>
